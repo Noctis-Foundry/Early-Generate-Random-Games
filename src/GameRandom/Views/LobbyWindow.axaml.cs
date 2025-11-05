82b154772846a568d91fb81a6f2217a92eeeba11
Ar0cka <120028878+Ar0cka@users.noreply.github.com>
2025-11-05 16:50:35 +1200

    Feat(UI) Style lobby (#14)
    Co-authored-by: TREN1R <MRcolur@yandex.com>

--- src/GameRandom/Views/LobbyWindow.axaml.cs
+++ src/GameRandom/Views/LobbyWindow.axaml.cs
@@ -1,9 +1,20 @@
 ﻿using System.ComponentModel;
+using System;
 using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using GameRandom.DataBaseContexts;
+using GameRandom.Scr.Events;
+using GameRandom.Scr.LobbySystem;
+using GameRandom.Scr.Service;
 using GameRandom.Scr.DI;
 using GameRandom.SteamSDK;
+using GameRandom.SteamSDK.Events;
+using GameRandom.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameRandom.Views.LobbyModalWindow;
 
@@ -11,16 +22,28 @@ public partial class LobbyWindow : Window
 {
     private const int MaxLenghtId = 18;
     private readonly LobbyService _lobbyService;
+    [Inject] private CreateLobbyService? _service;
+    [Inject] private EventBus? _eventBus;
     
     public LobbyWindow()
     {
+        Console.WriteLine("Initialize Create Lobby");
+        
         InitializeComponent();
+        
+        var viewModel = new CreateLobbyViewModel();
+        DataContext = viewModel;
+        
+        Di.Container.RegisterSingleInstance(viewModel);
         
         if (Di.Container.TryGetInstance<LobbyService>() is LobbyService system)
         {
             _lobbyService = system;
         }
+        
+        
     }
+    
     
     private void OnLobbyIdChanging(object sender, TextChangedEventArgs e)
     {
@@ -48,5 +71,21 @@ public partial class LobbyWindow : Window
         {
             _lobbyService.ConnectToLobby(id);
         }
+    }
+    
+    private void CreateNewLobby(object? sender, RoutedEventArgs e)
+    {
+        if (_service == null)
+            throw new Exception("Failed to create new lobby. Create lobby service is null");
+        
+        _service.CreateLobby();
+
+        if (_eventBus == null)
+        {
+            Logger.Error("CreateLobby: EventBus is null");
+            return;
+        }
+            
+        _eventBus.Publish(new LobbyUpdate());
     }
 }
