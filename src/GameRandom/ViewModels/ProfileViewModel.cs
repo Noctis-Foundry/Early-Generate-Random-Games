24c017bb9a62439f9183f1e16c41606f387cf3a6
Ar0cka <vanoshul@gmail.com>
2026-02-14 20:01:07 +1200

    feat(Profile): Update profile view module - Add async load table from database - Converter game progress to player table data - Unload class after closing

--- src/GameRandom/ViewModels/ProfileViewModel.cs
+++ src/GameRandom/ViewModels/ProfileViewModel.cs
@@ -1,35 +1,100 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Threading;
+using GameRandom.DataBaseContexts;
+using GameRandom.Scr.DI;
+using GameRandom.Scr.Service;
+using GameRandom.SteamSDK;
+using GameRandom.SteamSDK.Enums;
 
 namespace GameRandom.ViewModels;
 
 public class ProfileViewModel : ViewModelBase
 {
-    public ObservableCollection<Person> People { get; set; }
-
-    public ProfileViewModel()
+    [Inject] private DatabaseService? _database;
+    [Inject] private ErrorService? _errorService;
+    public ObservableCollection<ProfileTableData>? GameProgresses {get; set;}
+    
+    /// <summary>
+    /// Method starting operations and threads for load table with player games to profile table
+    /// </summary>
+    public async Task LoadTable()
     {
-        var people = new List<Person> 
-        {
-            new Person("Neil", 27, "Male"),
-            new Person("Buzz", 22, "Female"),
-            new Person("James", 18, "Male"),
-        };
+        Di.Container.ResolveFieldsFromClassInstance(this);
+        ulong userId = SteamManager.GetSteamManager().GetSteamId().m_SteamID;
         
-        People = new ObservableCollection<Person>(people);
+        var list = await _database.GetTableListAsync<GameProgress>();
+
+        if (list == null || list.Count == 0)
+        {
+            _errorService.ShowErrorWindow("Failed donwoload data from database. Game progress table is empty", ErrorEnum.Message);
+            return;
+        }
+        
+        var playerTable = ToPlayerTableData(list, userId);
+
+        if (playerTable == null)
+        {
+            _errorService.ShowErrorWindow($"Failed rebind data to player table data. Games for your client id: {userId} is not founded",
+                ErrorEnum.Message);
+            return;
+        }
+        
+        GameProgresses = new ObservableCollection<ProfileTableData>(playerTable);
+    }
+    
+    /// <summary>
+    /// Sorting GameProgress with client ID and converting to ProfileTableData
+    /// </summary>
+    /// <param name="gameProgress">List with all games</param>
+    /// <param name="userId">Steam client id</param>
+    /// <returns></returns>
+    private List<ProfileTableData>? ToPlayerTableData(List<GameProgress> gameProgress, ulong userId)
+    {
+        var list = gameProgress.Where(e => e.ClientId == userId).ToList();
+
+        if (list.Count == 0)
+            return null;
+
+        List<ProfileTableData> playerTable = new List<ProfileTableData>();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            playerTable.Add(new ProfileTableData(list[i].GameName, list[i].DataBegin, list[i].DataEnd));
+        }
+        
+        return playerTable;
+    }
+    
+    /// <summary>
+    /// Need for clear all reference after closing user content. call from main class with dispose method
+    /// </summary>
+    public void UnloadTable()
+    {
+        if (GameProgresses != null)
+        {
+            GameProgresses.Clear();
+            GameProgresses = null;
+        }
+        
+        _database = null;
+        _errorService = null;
     }
 }
 
-public class Person
+public class ProfileTableData
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Gender { get; set; }
+    public string GameName { get; set; }
+    public string DataBegin { get; set; }
+    public string DataEnd { get; set; }
 
-    public Person(string name, int age, string gender)
+    public ProfileTableData(string gameName, string dataBegin, string dataEnd)
     {
-        Name = name;
-        Age = age;
-        Gender = gender;
+        GameName = gameName;
+        DataBegin = dataBegin;
+        DataEnd = dataEnd;
     }
 }
