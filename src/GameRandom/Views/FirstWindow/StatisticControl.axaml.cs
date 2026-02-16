d248b7a9896555baf12291e9ecfdd0d169452a29
Ar0cka <vanoshul@gmail.com>
2026-02-16 23:30:20 +1200

    feat(Statistic): Add initialization item panel with statistic cards

--- src/GameRandom/Views/FirstWindow/StatisticControl.axaml.cs
+++ src/GameRandom/Views/FirstWindow/StatisticControl.axaml.cs
@@ -11,5 +11,18 @@ public partial class StatisticControl : UserControl
     public StatisticControl()
     {
         InitializeComponent();
+        DataContext = new StatisticViewModel();
+    }
+
+    public void Open()
+    {
+        if (DataContext is StatisticViewModel statisticViewModel)
+            Dispatcher.UIThread.InvokeAsync(async () => await statisticViewModel.LoadStatisticAsync());
+    }
+
+    public void Close()
+    {
+        if (DataContext is StatisticViewModel statisticViewModel)
+            statisticViewModel.Dispose();
     }
 }
