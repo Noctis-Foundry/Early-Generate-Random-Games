d81f1f55f50cc55c9c8d77f89ea81fe36d546e72
Ar0cka <vanoshul@gmail.com>
2026-02-18 23:58:58 +1200

    feat(Logic): Add view model for current game status without logic

--- src/GameRandom/ViewModels/CurrentGameStatusViewModel.cs
+++ src/GameRandom/ViewModels/CurrentGameStatusViewModel.cs
@@ -1,6 +1,19 @@
+using GameRandom.Scr.DI;
+using GameRandom.Scr.Service;
+
 namespace GameRandom.ViewModels;
 
-public class CurrentGameStatusViewModel
+public class CurrentGameStatusViewModel : ViewModelBase
+{
+    [Inject] private DatabaseService? database = null!;
+
+    public void LoadInfo(int appId)
+    {
+        
+    }
+}
+
+public class GameStatusInfo()
 {
     
 }
