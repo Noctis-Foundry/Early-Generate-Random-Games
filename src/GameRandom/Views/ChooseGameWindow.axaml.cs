6df2f706f0c48c5ed01ada268ef282c3bfe12431
Ar0cka <vanoshul@gmail.com>
2026-02-19 19:27:18 +1200

    project: update project includes and json file

---
+++ src/GameRandom/Views/ChooseGameWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GameRandom.Views;
+
+public partial class ChooseGameWindow : Window
+{
+    public ChooseGameWindow()
+    {
+        InitializeComponent();
+    }
+}
