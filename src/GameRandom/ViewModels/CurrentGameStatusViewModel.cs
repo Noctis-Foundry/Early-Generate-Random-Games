ff6ed91fed0896acd3bb963be11acf9ff3e459a9
Ar0cka <vanoshul@gmail.com>
2026-02-24 02:09:52 +1200

    feat(CurrentGameWindow): Updating initialization logic - Add dispatcher timer for update left time - Add logic for updating textblocks after initialization

--- src/GameRandom/ViewModels/CurrentGameStatusViewModel.cs
+++ src/GameRandom/ViewModels/CurrentGameStatusViewModel.cs
@@ -6,7 +6,11 @@ using GameRandom.Scr.DI;
 using GameRandom.Scr.Service;
 using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using Avalonia.Threading;
 using GameRandom.DataBaseContexts;
+using GameRandom.Service;
+using GameRandom.SteamSDK;
+using GameRandom.SteamSDK.Enums;
 using GameRandom.SteamSDK.UserData;
 
 namespace GameRandom.ViewModels;
@@ -15,12 +19,12 @@ public class CurrentGameStatusViewModel : ViewModelBase
 {
     [Inject] private DatabaseService? _databaseService = null!;
 
-    private Timer _timer;
+    private DispatcherTimer _timer;
     private UpdatingInfo _updatingInfo;
 
     public async Task LoadInfo(GameStatusInfo uiInfo)
     {
-        Di.Container.ResolveField(out _databaseService);
+        Di.Container.ResolveFieldsFromClassInstance(this);
         
         var userInfo = User.GetInstance().GetUserInfo();
 
@@ -30,12 +34,25 @@ public class CurrentGameStatusViewModel : ViewModelBase
 
         if (userGameInfo is not null && userGameInfo.AppId != 0)
         {
-            var gameInfo = userGameInfo.GameProgresses;
+            var gameInfo = await _databaseService.GetFirstOrDefaultAsync<GameProgresses>
+                (e => e.AppId == userGameInfo.AppId);
 
+            if (gameInfo is null)
+            {
+                Console.WriteLine($"Failed to get gameInfo with appID: {userGameInfo.AppId} from database");
+                throw new NullReferenceException();
+            }
+            
             _updatingInfo = new UpdatingInfo(gameInfo.EndTime, uiInfo.GetTimeSpentBlock());
 
-            _timer = new Timer(1000);
-            _timer.Elapsed += (sender, e) => _updatingInfo.CalculateTime();
+            _updatingInfo.CalculateTime();
+            
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(1000);
+            _timer.Tick += (sender, args) => _updatingInfo.CalculateTime(); 
+            _timer.Start();
+            
+            uiInfo.UpdateInfo(gameInfo.AppName, gameInfo.BeginTime, DateTime.Now, gameInfo.EndTime, SteamService.Instance.GetImageSyncFromBytes(gameInfo.AppHeaderImage));
         }
     }
 
@@ -43,6 +60,7 @@ public class CurrentGameStatusViewModel : ViewModelBase
     {
         _databaseService = null;
         
+        _timer.Stop();
     }
 }
 
@@ -55,13 +73,14 @@ public class GameStatusInfo(TextBlock nameBlock, TextBlock beginTime, TextBlock 
     private readonly TextBlock _endTime = endTime;
     private readonly Image _gameImage = gameImage;
 
-    public void UpdateInfo(string name, DateTime beginTime, DateTime todayDate, DateTime endTime, Bitmap gameImage)
+    public void UpdateInfo(string? name, DateTime beginTime, DateTime todayDate, DateTime endTime, Bitmap? gameImage)
     {
-        _nameBlock.Text = name;
-        _beginTime.Text = beginTime.ToString("D");
-        _endTime.Text = endTime.ToString("D");
-        _todayDate.Text = todayDate.ToString("D");
-        _gameImage.Source = gameImage;
+        _nameBlock.Text = name ?? "Default";
+        _beginTime.Text = "Start time: " + beginTime.ToString("D");
+        _endTime.Text =  "End time: " + endTime.ToString("D");
+        _todayDate.Text = "Today: " + todayDate.ToString("D");
+        
+        _gameImage.Source = gameImage ?? new Bitmap("Assets/steamAwatarWithNight.jpg");
     }
 
     public TextBlock GetTimeSpentBlock()
@@ -80,6 +99,6 @@ public class UpdatingInfo(DateTime endTime, TextBlock timeSpent)
         var timeNow = DateTime.Now;
         var timeLeft = _endTime - timeNow;
         
-        _timeSpent.Text = timeLeft.ToString($"{timeLeft.Days}d/{timeLeft.Hours}h/{timeLeft.Minutes}m/{timeLeft.Seconds}");
+        _timeSpent.Text = $"Left Time: {timeLeft.Days}d/{timeLeft.Hours}h/{timeLeft.Minutes}m/{timeLeft.Seconds}s";
     }
 }
