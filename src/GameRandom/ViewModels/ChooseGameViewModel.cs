6c22ca22ac57aa8a672b43725777d2311b449e5c
Ar0cka <vanoshul@gmail.com>
2026-02-25 01:44:06 +1200

    feat(ChooseGameWindow): Update logic for choose game - Update choose game window style - Add method for initialize window from rool game - Add view model for choose game window - Extract choose game logic from roll game to choose game window view   model - Add load ui info method - Add show store page button - Add choose game button

---
+++ src/GameRandom/ViewModels/ChooseGameViewModel.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using GameRandom.CoreApp;
+using GameRandom.DataBaseContexts;
+using GameRandom.Scr.DI;
+using GameRandom.Scr.Service;
+using GameRandom.Service;
+using GameRandom.SteamSDK;
+using GameRandom.SteamSDK.Enums;
+using GameRandom.SteamSDK.UserData;
+
+namespace GameRandom.ViewModels;
+
+public class ChooseGameViewModel : ViewModelBase, IDisposable
+{
+    private ChooseGameUiInfo? _uiInfo;
+    private AppSavedContext? _savedContext;
+    private byte[]? _imageBytes;
+    
+    public async Task<bool> ChooseGame()
+    {
+        Console.WriteLine("Choose Game");
+
+        if (_savedContext is null || _imageBytes is null)
+        {
+            throw new NullReferenceException($"_savedContext or _imageBytes is null. _imageBytes {_imageBytes == null}, _saveContext {_savedContext == null}");
+        }
+        
+        DateTime date = DateTime.UtcNow;
+        DateTime endDate = date.AddDays(30);
+
+        var gameInfo = new GameProgresses
+        {
+            AppHeaderImage = _imageBytes,
+            AppId = _savedContext.AppId,
+            AppName = _savedContext.AppName,
+            BeginTime = date,
+            Comment = "Default",
+            EndTime = endDate,
+            Grade = 0,
+            IsFinished = false,
+            FinishTime = endDate,
+            PlayerId = SteamManager.GetSteamIdAsLong()
+        };
+
+        if (Di.Container.GetInstance<DatabaseService>() is DatabaseService service)
+        {
+            UserGame? userGame = await service.GetUserGameAsync(User.GetInstance().GetUserInfo());
+
+            if (userGame is null)
+                throw new NullReferenceException("User game is not initialize");
+
+            if (userGame.AppId != 0)
+            {
+                if (Di.Container.GetInstance<ErrorService>() is ErrorService errorService)
+                {
+                    errorService.ShowErrorWindow("Failed to set new game. Finish your current game", ErrorEnum.Message);
+                    return false;
+                }
+            }
+            
+            bool isAdded = await service.AddItemAsync(gameInfo);
+            
+            if (!isAdded)
+                throw new Exception("Error add item to db");
+
+            userGame.AppId = _savedContext.AppId;
+            await service.UpdateAsync(userGame);
+        }
+
+        return true;
+    }
+
+    public void LoadGameInfo(AppSavedContext appSavedContext, ChooseGameUiInfo uiInfo, byte[] imageBytes)
+    {
+        _uiInfo = uiInfo;
+        _savedContext = appSavedContext;
+        _imageBytes = imageBytes;
+        
+        uiInfo.AppName.Text = appSavedContext.AppName;
+        uiInfo.Genres.Text = string.Join(", ", appSavedContext.AppGenres.Values);
+        uiInfo.DateRelease.Text = appSavedContext.AppReleaseYear.ToString();
+        uiInfo.GameHeaderImage.Source = SteamService.Instance.GetImageSyncFromBytes(_imageBytes);
+    }
+
+    public void ShowSteamStore()
+    {
+        var url = $"https://store.steampowered.com/app/{_savedContext?.AppId}";
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        });
+    }
+
+    public void Dispose()
+    {
+        _uiInfo = null;
+    }
+}
+
+public class ChooseGameUiInfo(
+    TextBlock appName,
+    TextBlock genres,
+    TextBlock dateRelease,
+    TextBlock rating,
+    TextBlock developers,
+    Image gameHeaderImage)
+{
+    public TextBlock AppName { get; private set; } = appName;
+    public TextBlock Genres { get; private set; } = genres;
+    public TextBlock DateRelease { get; private set; } = dateRelease;
+    public Image GameHeaderImage { get; private set; } = gameHeaderImage;
+}
--- src/GameRandom/Views/ChooseGameWindow.axaml.cs
+++ src/GameRandom/Views/ChooseGameWindow.axaml.cs
@@ -1,14 +1,82 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using GameRandom.CoreApp;
+using GameRandom.Scr.DI;
+using GameRandom.Service;
 using GameRandom.SteamSDK;
+using GameRandom.SteamSDK.Enums;
+using GameRandom.ViewModels;
 
 namespace GameRandom.Views;
 
 public partial class ChooseGameWindow : WindowAbstract
 {
+    [Inject] private ErrorService _errorService = null!;
+    
     public ChooseGameWindow()
     {
         InitializeComponent();
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        DataContext = new ChooseGameViewModel();
+
+        if (Design.IsDesignMode)
+            return;
+
+        Di.Container.ResolveField(out _errorService);
+    }
+
+    public void LoadData(AppSavedContext appSavedContext, byte[] imageBytes)
+    {
+        if (DataContext is ChooseGameViewModel viewModel)
+        {
+            ChooseGameUiInfo uiInfo = new ChooseGameUiInfo(GameName, GameGenres, ReleaseDate, GameRating,
+                GameDevelopers, GameHeader);
+            
+            viewModel.LoadGameInfo(appSavedContext, uiInfo, imageBytes);
+        }
+    }
+
+    public override void CloseWindow()
+    {
+        base.CloseWindow();
+        if (DataContext is ChooseGameViewModel viewModel)
+            viewModel.Dispose();
+    }
+    
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        base.OnClosing(e);
+        if (DataContext is ChooseGameViewModel viewModel)
+            viewModel.Dispose();
+    }
+
+
+    private void ToSteamStorePage(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is ChooseGameViewModel viewModel)
+            viewModel.ShowSteamStore();
+    }
+
+    private async void ChooseGame(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (DataContext is not ChooseGameViewModel viewModel) return;
+            
+            bool isAdd = await viewModel.ChooseGame();
+
+            if (!isAdd)
+                _errorService.ShowErrorWindow("Failed to add game to database, try again", ErrorEnum.Message);
+            else
+                _errorService.ShowErrorWindow("Game added to database", ErrorEnum.Message);
+
+        }
+        catch (Exception exception)
+        {
+            throw new Exception("Failed to add game progress to database: " + exception.Message);
+        }
     }
 }
