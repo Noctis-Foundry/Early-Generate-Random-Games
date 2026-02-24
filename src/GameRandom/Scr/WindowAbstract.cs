725632609ca1c104c33682371b8c8c13b0d3cf55
Ar0cka <vanoshul@gmail.com>
2026-02-24 18:54:58 +1200

    feat(Window): Add abstract window with virtual Open and Close and ovveride OnClosing to hide format

--- src/GameRandom/Scr/WindowAbstract.cs
+++ src/GameRandom/Scr/WindowAbstract.cs
@@ -1,6 +1,36 @@
+using Avalonia.Controls;
+
 namespace GameRandom.SteamSDK;
 
-public class WindowAbstract
+public abstract class WindowAbstract : Window
 {
+    public bool IsActive { get; private set; } = false;
+
+    public virtual void Open(Window? parent = null)
+    {
+        if (IsActive) return;
+        
+        IsActive = true;
+
+        if (parent is not null)
+        {
+            Show(parent);
+            return;
+        }
+        
+        Show();
+    }
     
+    public virtual void Close()
+    {
+        Hide();
+        IsActive = false;
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        Hide();
+        IsActive = false;
+        e.Cancel = true;
+    }
 }
