c88a99b0763cfcb00ccaa4d43eddfe788d65cd90
Ar0cka <vanoshul@gmail.com>
2026-02-26 03:03:54 +1200

    feat(Proj): Add target path to linux steam lib

---
+++ src/GameRandom/Views/FilterGameWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GameRandom.Views;
+
+public partial class FilterGameWindow : Window
+{
+    public FilterGameWindow()
+    {
+        InitializeComponent();
+    }
+}
