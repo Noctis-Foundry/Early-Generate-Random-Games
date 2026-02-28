4d7aa2911beca2b6be87e3b175b6f03f2cd6619b
Ar0cka <vanoshul@gmail.com>
2026-03-01 08:27:53 +1200

    feat(RollGameViewModel): Refactoring to MVVP patern - Extract UI logic from model - Deleted semaphore from class - Add token connection for generate random games - Add filter logic in class from FilterData

--- src/GameRandom/ViewModels/RollGameViewModel.cs
+++ src/GameRandom/ViewModels/RollGameViewModel.cs
@@ -1,7 +1,111 @@
-﻿
+﻿using System;
+using GameRandom.SteamSDK;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using GameRandom.CoreApp;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 
 namespace GameRandom.ViewModels;
 
-public class RollGameViewModel : ViewModelBase
+public class RollGameViewModel : ViewModelBase, IDisposable
 {
+    private List<AppInfo> _appInfo = new();
+    public List<AppInfo> AppInfo => _appInfo;
+    
+    private IGenApp? _generateRandomApps;
+    
+    private const int IterationLimit = 500;
+    
+    private int _iterationCount;
+
+    private bool _isFilter = false;
+
+    public bool IsFilter
+    {
+        get => _isFilter;
+        set => SetProperty(ref _isFilter, value);
+    }
+
+    public RollGameViewModel()
+    {
+        if (Design.IsDesignMode) return;
+        
+        _generateRandomApps = new GenerateRandomApps();
+    }
+
+    public async Task GenerateGames(int countGames, FilteredData? filteredGamesData, CancellationToken cancellationToken = default)
+    {
+        if (!IsValidationGenerateData())
+            return;
+
+        ClearItems();
+
+        try
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            while (_iterationCount < IterationLimit && _appInfo.Count < countGames)
+            {
+                _iterationCount++;
+
+                var gameInfo = _generateRandomApps?.GetRandomGame();
+
+                if (gameInfo is null || _appInfo.Any(e => e.AppData.AppId == gameInfo.AppId))
+                    continue;
+
+                if (IsFilter && filteredGamesData is not null)
+                    if (!FilterGame(gameInfo, filteredGamesData))
+                        continue;
+
+                var imageBytes = await SteamService.Instance.GetImageBytes(gameInfo.HeaderImage);
+
+                if (imageBytes == null)
+                    continue;
+
+                _appInfo.Add(new AppInfo(gameInfo, imageBytes));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    
+    /// <summary>
+    /// Confirms with user before stopping ongoing game roll operation.
+    /// </summary>
+    /// <returns>True if operation can proceed, false if canceled.</returns>
+
+    private bool FilterGame(AppSavedContext savedGame, FilteredData filter)
+    {
+        if (filter.Categories.Count > 0 && !filter.Categories.Any(c => savedGame.AppCategoris.Contains(c)))
+            return false;
+
+        if (filter.Genres.Count > 0 && !filter.Genres.Any(g => savedGame.AppGenres.Contains(g)))
+            return false;
+
+        if (filter.Years.Count > 0 && !filter.Years.Any(y => y == savedGame.AppReleaseYear))
+            return false;
+
+        return true;
+    }
+
+    private bool IsValidationGenerateData()
+    {
+        return _generateRandomApps.IsInitialized;
+    }
+
+    private void ClearItems()
+    {
+        _appInfo.Clear();
+        _iterationCount = 0;
+    }
+
+    public void Dispose()
+    {
+        _generateRandomApps = null;
+        ClearItems();
+    }
 }
