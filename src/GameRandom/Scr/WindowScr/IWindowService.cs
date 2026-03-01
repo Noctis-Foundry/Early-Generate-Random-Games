20365a227f2e5789bf91e46bd901135490b6a5b0
Ar0cka <vanoshul@gmail.com>
2026-03-02 05:17:30 +1200

    delete(IWindowService)

--- src/GameRandom/Scr/WindowScr/IWindowService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using Avalonia.Controls;
-
-namespace GameRandom.Scr.WindowScr;
-
-public interface IWindowService
-{
-    Task ShowDialogAsync<TWindow>() where TWindow : Window, new();
-}
