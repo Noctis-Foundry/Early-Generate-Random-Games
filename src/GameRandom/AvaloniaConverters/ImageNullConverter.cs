b0e52534ca93ac757c79226cfcb996b2180da56e
Ar0cka <vanoshul@gmail.com>
2026-03-04 06:06:56 +1200

    feat(AvaloniaTools): Add converters for Image and TimeSpan formats

--- src/GameRandom/AvaloniaConverters/ImageNullConverter.cs
+++ src/GameRandom/AvaloniaConverters/ImageNullConverter.cs
@@ -1,6 +1,23 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
 namespace GameRandom.AvaloniaConverters;
 
-public class ImageNullConverter
+public class ImageNullConverter : IValueConverter
 {
-    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return new Bitmap(AssetLoader.Open(new Uri("avares://GameRandom/Assets/steamAwatarWithNight.jpg")));
+        
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/GameRandom/AvaloniaConverters/TimeSpanFormatConverter.cs
+++ src/GameRandom/AvaloniaConverters/TimeSpanFormatConverter.cs
@@ -1,6 +1,24 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
 namespace GameRandom.AvaloniaConverters;
 
-public class TimeSpanFormatConverter
+public class TimeSpanFormatConverter : IValueConverter
 {
-    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            string prefix = parameter as string ?? "N/A";
+            return $"{prefix}: {dateTime:D}";
+        }
+        
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
