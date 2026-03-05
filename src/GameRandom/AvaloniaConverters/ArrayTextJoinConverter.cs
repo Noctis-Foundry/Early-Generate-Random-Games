cb3dfe04c028f18e5f6212ab65ba91cd783012f4
Ar0cka <vanoshul@gmail.com>
2026-03-06 05:30:57 +1200

    feat(Converters): Add converters for HashSet<string> to string

--- src/GameRandom/AvaloniaConverters/ArrayTextJoinConverter.cs
+++ src/GameRandom/AvaloniaConverters/ArrayTextJoinConverter.cs
@@ -1,6 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
 namespace GameRandom.AvaloniaConverters;
 
-public class ArrayTextJoin
+public class ArrayTextJoinConverter : IValueConverter
 {
-    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is HashSet<string> array)
+        {
+            string prefix = parameter as string ?? "N/A";
+            string text = string.Join(", ", array);
+            return $"{prefix}: {text}";
+        }
+
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
