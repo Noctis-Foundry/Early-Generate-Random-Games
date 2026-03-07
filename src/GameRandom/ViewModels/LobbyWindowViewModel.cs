ca371c9c1d5c1ab17899da2454d9a34a4bc8953d
Ar0cka <vanoshul@gmail.com>
2026-03-08 04:05:02 +1200

    fix(LobbyWindow): Rename lobby window view model

---
+++ src/GameRandom/ViewModels/LobbyWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia.Controls;
+using GameRandom.Events;
+using GameRandom.Scr.DI;
+using GameRandom.Scr.Events;
+using GameRandom.SteamSDK.UserData;
+
+namespace GameRandom.ViewModels;
+
+public class LobbyWindowViewModel : ViewModelBase
+{
+    private const long  DefaultIdMessage = 0;
+
+    private long  _currentLobbyId;
+
+    public long CurrentLobbyID
+    {
+        get => _currentLobbyId;
+        set => SetProperty(ref _currentLobbyId, value);
+    }
+
+    public LobbyWindowViewModel()
+    {
+        if (Design.IsDesignMode)
+            return;
+        
+        GetCurrentId();
+
+        if (Di.Container.GetInstance<EventBus>() is EventBus eventBus)
+        {
+            eventBus.Subscribe<LobbyUpdate>(e => GetCurrentId());
+        }
+    }
+
+    private void GetCurrentId()
+    {
+        var userInfo = User.GetInstance().GetUserInfo();
+        CurrentLobbyID = userInfo.LobbyId > 0 ? userInfo.LobbyId: DefaultIdMessage;
+    }
+}
