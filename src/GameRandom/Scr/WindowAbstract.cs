afde621ab795406f06b85edbe79d1decbf761ac8
Ar0cka <vanoshul@gmail.com>
2026-03-10 14:32:36 +1200

    refactor: Add saving closing for base AbstractWindow methods

--- src/GameRandom/Scr/WindowAbstract.cs
+++ src/GameRandom/Scr/WindowAbstract.cs
@@ -5,12 +5,14 @@ namespace GameRandom.SteamSDK;
 public abstract class WindowAbstract : Window
 {
     public bool IsActive { get; private set; } = false;
+    protected bool IsClosing = false;
 
     public virtual void Open(Window? parent = null)
     {
         if (IsActive) return;
         
         IsActive = true;
+        IsClosing = false;
 
         if (parent is not null)
         {
@@ -23,14 +25,20 @@ public abstract class WindowAbstract : Window
     
     public virtual void CloseWindow()
     {
+        if (IsClosing) return;
+        
         Hide();
         IsActive = false;
+        IsClosing = false;
     }
 
     protected override void OnClosing(WindowClosingEventArgs e)
     {
+        if (IsClosing) return;
+        
         Hide();
         IsActive = false;
         e.Cancel = true;
+        IsClosing = false;
     }
 }
