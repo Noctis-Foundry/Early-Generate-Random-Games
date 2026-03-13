6c76e60b236c2bd1a8e4863d6b01d1aa2050737d
Ar0cka <vanoshul@gmail.com>
2026-03-14 08:40:12 +1200

    feat(AdminRegistation): Add base structure for appointment new admins

--- src/GameRandom/ViewModels/AdminSystem/AdminRegistrationViewModel.cs
+++ src/GameRandom/ViewModels/AdminSystem/AdminRegistrationViewModel.cs
@@ -1,6 +1,28 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using GameRandom.DataBaseContexts;
+using GameRandom.Scr.DI;
+using GameRandom.Scr.Service;
+
 namespace GameRandom.ViewModels.AdminSystem;
 
-public class AdminRegistrationViewModel
+public class AdminRegistrationViewModel : ViewModelBase
 {
+    [Inject] private DatabaseService? _databaseService;
     
+    private ObservableCollection<Users> _admins;
+    public ObservableCollection<Users> Admins
+    {
+        get => _admins;
+        set => SetProperty(ref _admins, value);
+    }
+
+    public async Task LoadData()
+    {
+        var users = await _databaseService.GetTableListAsync<Users>();
+        
+        if (users is null)
+            throw new NullReferenceException(nameof(users));
+    }
 }
--- src/GameRandom/Views/AdminRegistrationWindow.axaml.cs
+++ src/GameRandom/Views/AdminRegistrationWindow.axaml.cs
@@ -1,10 +1,11 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using GameRandom.SteamSDK;
 
 namespace GameRandom.Views;
 
-public partial class AdminRegistrationWindow : Window
+public partial class AdminRegistrationWindow : WindowAbstract
 {
     public AdminRegistrationWindow()
     {
