5a188432e1f8a95326e3a59b202601b1e0cf5485
Ar0cka <vanoshul@gmail.com>
2026-04-06 01:43:01 +1200

    refactor: reorganize files and add ProfileViewModel
    - Rename FilterGameActions.cs to LoadFilterData.cs for clarity
    - Add ProfileViewModel for profile management
    - Improve naming consistency across ViewModels

---
+++ src/GameRandom/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,59 @@
+using Avalonia.Media.Imaging;
+using GameRandom.Service;
+using GameRandom.Src;
+using GameRandom.ViewModels.BaseClasses;
+using Steamworks;
+
+namespace GameRandom.ViewModels;
+
+using ViewModelBase = ViewModelBase;
+
+public class ProfileViewModel : ViewModelBase
+{
+    private Bitmap? _profileImage;
+    public Bitmap? ProfileImage
+    {
+        get => _profileImage;
+        set => SetProperty(ref _profileImage, value);
+    }
+
+    private string? _playerNickname;
+    public string? PlayerNickname
+    {
+        get => _playerNickname;
+        set => SetProperty(ref _playerNickname, value);
+    }
+    
+    public ProfileViewModel()
+    {
+        InitializeProfileContent();
+    }
+
+    private void InitializeProfileContent()
+    {
+        CSteamID steamId = SteamManager.GetSteamManager().GetSteamId();
+        
+        string accName = SteamFriends.GetPersonaName();
+        
+        int imageId = SteamFriends.GetLargeFriendAvatar(steamId);
+        
+        var bitmap = AvaloniaService.Instance.CreateSteamImage(imageId);
+        
+        if (string.IsNullOrEmpty(accName))
+            accName = "Unknown";
+        
+        if (bitmap is null)
+            bitmap = AvaloniaService.Instance.DefaultUserImage();
+        
+        ProfileImage = bitmap;
+        PlayerNickname = accName;
+    }
+
+    public override void Dispose()
+    {
+        ProfileImage?.Dispose();
+        PlayerNickname = null;
+        
+        base.Dispose();
+    }
+}
